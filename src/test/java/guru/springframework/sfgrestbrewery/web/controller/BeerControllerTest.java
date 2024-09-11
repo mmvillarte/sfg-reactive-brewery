721594fa9207230be00022ba03800537ff07a97e
Maria-del-Mar <mmvillarte@hotmail.com>
2024-09-11 11:08:04 +0200

    Task 4 - Assignment - Update Get Operations to Reactive Types

--- src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgrestbrewery.web.controller;
 import guru.springframework.sfgrestbrewery.bootstrap.BeerLoader;
 import guru.springframework.sfgrestbrewery.services.BeerService;
 import guru.springframework.sfgrestbrewery.web.model.BeerDto;
+import guru.springframework.sfgrestbrewery.web.model.BeerPagedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.BDDMockito.given;
 
 @WebFluxTest(BeerController.class)
@@ -28,6 +31,8 @@ class BeerControllerTest {
 
     BeerDto defaultBeerDto;
 
+    BeerPagedList defaulBeerPagedList;
+
     @BeforeEach
     void setUp() {
         defaultBeerDto = BeerDto.builder()
@@ -35,6 +40,8 @@ class BeerControllerTest {
                 .beerStyle("PALE_ALE")
                 .upc(BeerLoader.BEER_1_UPC)
                 .build();
+
+        defaulBeerPagedList = new BeerPagedList(List.of(defaultBeerDto));
     }
 
     @Test
@@ -44,6 +51,32 @@ class BeerControllerTest {
 
         webTestClient.get()
                 .uri("/api/v1/beer/" + beerId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerDto.class)
+                .value(beerDto -> beerDto.getBeerName(), equalTo(defaultBeerDto.getBeerName()));
+    }
+
+    @Test
+    void listBeers() {
+        given(beerService.listBeers(any(), any(), any(), any())).willReturn(defaulBeerPagedList);
+
+        webTestClient.get()
+                .uri("/api/v1/beer/")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerPagedList.class)
+                .value(beerPagedList -> beerPagedList.get().findFirst().get().getBeerName(), equalTo(defaultBeerDto.getBeerName()));
+    }
+
+    @Test
+    void getBeerByUpc() {
+        given(beerService.getByUpc(any())).willReturn(defaultBeerDto);
+
+        webTestClient.get()
+                .uri("/api/v1/beerUpc/" + BeerLoader.BEER_1_UPC)
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
