b9d5c7609b416379ef9ce5b0ddaba509f13b99df
Maria-del-Mar <mmvillarte@hotmail.com>
2024-09-11 10:25:43 +0200

    WebTestClient

---
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
@@ -0,0 +1,53 @@
+package guru.springframework.sfgrestbrewery.web.controller;
+
+import guru.springframework.sfgrestbrewery.bootstrap.BeerLoader;
+import guru.springframework.sfgrestbrewery.services.BeerService;
+import guru.springframework.sfgrestbrewery.web.model.BeerDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@WebFluxTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerDto defaultBeerDto;
+
+    @BeforeEach
+    void setUp() {
+        defaultBeerDto = BeerDto.builder()
+                .beerName("Test beer")
+                .beerStyle("PALE_ALE")
+                .upc(BeerLoader.BEER_1_UPC)
+                .build();
+    }
+
+    @Test
+    void getBeerById() {
+        UUID beerId = UUID.randomUUID();
+        given(beerService.getById(any(), any())).willReturn(defaultBeerDto);
+
+        webTestClient.get()
+                .uri("/api/v1/beer/" + beerId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerDto.class)
+                .value(beerDto -> beerDto.getBeerName(), equalTo(defaultBeerDto));
+    }
+}
