fb3c205d52a3a8c32064bff2f9befe1def7f73eb
Maria-del-Mar <mmvillarte@hotmail.com>
2024-09-12 16:38:40 +0200

    Validate Save New Beer

--- src/main/java/guru/springframework/sfgrestbrewery/web/functional/BeerHandlerV2.java
+++ src/main/java/guru/springframework/sfgrestbrewery/web/functional/BeerHandlerV2.java
@@ -4,9 +4,14 @@ import guru.springframework.sfgrestbrewery.services.BeerService;
 import guru.springframework.sfgrestbrewery.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -14,6 +19,8 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class BeerHandlerV2 {
     private final BeerService beerService;
+
+    private final Validator validator;
 
     public Mono<ServerResponse> getBeerById(ServerRequest request){
         Integer beerId = Integer.valueOf(request.pathVariable("beerId"));
@@ -35,7 +42,8 @@ public class BeerHandlerV2 {
     }
 
     public Mono<ServerResponse> saveNewBeer(ServerRequest request){
-        Mono<BeerDto> beerDtoMono = request.bodyToMono(BeerDto.class);
+        Mono<BeerDto> beerDtoMono = request.bodyToMono(BeerDto.class)
+                .doOnNext(this::validate);
 
         return beerService.saveNewBeerMono(beerDtoMono)
                 .flatMap(beerDto -> {
@@ -44,5 +52,15 @@ public class BeerHandlerV2 {
                             .build();
 
                 });
+    }
+
+    private void validate(BeerDto beerDto) {
+        Errors errors = new BeanPropertyBindingResult(beerDto, "beerDto");
+
+       validator.validate(beerDto, errors);
+
+       if(errors.hasErrors()) {
+           throw new ServerWebInputException(errors.toString());
+       }
     }
 }
